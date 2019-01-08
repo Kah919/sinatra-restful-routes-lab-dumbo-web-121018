ba5696b7743367ce3667a1e050543a0b8d42f127
github username <github email address>
2019-01-08 18:05:50 -0500

    First commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -5,5 +7,40 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipe' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipe/new' do
+    erb :new_recipe
+  end
+
+  post '/recipe' do
+    recipe = Recipe.create(params)
+    redirect "/recipe/#{recipe.id}"
+  end
+
+  get '/recipe/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show_recipe
+  end
+
+  get '/recipe/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit_recipe
+  end
+
+  patch "/recipe/:id" do
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipe/#{@recipe.id}"
+  end
+
+  delete "/recipe/:id" do
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect "/recipe"
+  end
 
 end
